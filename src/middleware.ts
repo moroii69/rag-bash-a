aa384f43afbf34d98750b638c16921c6ca819f06
moroii69 <ufraansiddiqui30@gmail.com>
2025-10-13 01:27:07 +0530

    add global types and admin role check in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
 	"/",
 	"/sign-in(.*)?",
 	"/sign-up(.*)?",
 ]);
+const isAdminRoute = createRouteMatcher([
+	"/upload",
+	// "/admin(.*)?",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
+	const { sessionClaims } = await auth();
+	const isAdmin = sessionClaims?.metadata.roles?.includes("admin");
+
+	if (isAdminRoute(req) && !isAdmin) {
+		const url = new URL("/", req.url);
+		return NextResponse.redirect(url);
+	}
 	if (!isPublicRoute(req)) {
 		await auth.protect();
 	}
