6525c1703c4719aa30e058d593190ea3121cbc3b
moroii69 <ufraansiddiqui30@gmail.com>
2025-10-12 19:23:26 +0530

    initial commit with shadcn ui and ai elements componenets

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,20 @@
+import { streamText, UIMessage, convertToModelMessages } from "ai";
+import { google } from "@ai-sdk/google";
+
+export async function POST(req: Request) {
+
+    try {	const { messages }: { messages: UIMessage[] } = await req.json();
+
+	const result = streamText({
+		model: "gemini-2.5-flash-lite",
+		messages: convertToModelMessages(messages),
+	});
+
+	return result.toUIMessageStreamResponse();
+
+
+    } catch (error) {
+        console.error("error streaming chat completion", error);
+        return new Response("Error streaming chat completion", { status: 500 });
+    }
+}
